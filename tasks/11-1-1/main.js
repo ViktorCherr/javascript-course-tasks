ed8e4154013fe245d3b45131a226d82206a9c889
majgikhina <mariya.ajgikhina@triafly.ru>
2024-02-14 14:46:07 +0300

    add new tak

---
+++ tasks/11-1-1/main.js
@@ -0,0 +1,29 @@
+/*
+Давайте представим, что вы разрабатываете приложение для учета библиотеки книг.
+
+Вам нужно реализовать ряд функций:
+- функцию addBook(), которая запускается по клику на кнопку.
+- функцию, которая будет отслеживать количество книг каждого жанра в библиотеке с использованием объекта Map.
+- функцию, которая будет перерисовывать статистику.
+- функцию, которая будет перерисовывать историю.
+- опционально: проверки/очистка ввода и все, что вам еще захочется и потребуется.
+*/
+const genreStats = new Map();
+
+function addBook() {
+    const title = document.getElementById('bookTitle').value;
+    const genre = document.getElementById('genre').value;
+
+}
+
+function updateStats(genre) {
+
+}
+
+function updateViewHistory(title, genre){
+
+}
+
+function updateViewStats(){
+
+}
