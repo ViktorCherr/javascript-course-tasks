1e2f4d7234efc725607cef4e48f9bbaf026f6e93
majgikhina <mariya.ajgikhina@triafly.ru>
2024-02-24 17:58:18 +0300

    refactor 9-5-2

--- tasks/9-5-2/main.js
+++ tasks/9-5-2/main.js
@@ -3,3 +3,44 @@
 Необходимо добавить поле ввода, которое будет фильтровать 
 список студентов по введенному тексту.
 */
+
+const students = [
+    'Алексей',
+    'Ирина',
+    'Максим',
+    'Наталья',
+    'Ольга',
+    'Сергей',
+    // ... добавьте еще имен по желанию
+];
+
+// Назначаем обработчик события ввода на поле
+document.getElementById('filter-input').addEventListener('input', handleFilter);
+
+// Изначально отображаем всех студентов
+showStudents(students);
+
+// Функция обработки события ввода и фильтрации
+function handleFilter(event) {
+    // Получаем значение из поля ввода (фильтр)
+    const filterValue = event.target.value.trim().toLowerCase();
+    // Очищаем список студентов на веб-странице
+    clearStudentsList();
+
+    // Перебираем массив "students" и отображаем только отфильтрованных студентов
+}
+
+// Функция отображения всех студентов
+function showStudents(studentsArray) {
+
+}
+
+// Функция добавления студента в список на веб-странице
+function addStudentToList(student) {
+
+}
+
+// Функция очистки списка студентов на веб-странице
+function clearStudentsList() {
+
+}
