997716f2a3ae7cc0f4b2eea488384dfc7ddfa7e1
admin <Janpole12@yandex.ru>
2024-03-12 22:40:25 +0300

    3-8-1

--- tasks/3-8-1/main.js
+++ tasks/3-8-1/main.js
@@ -8,6 +8,6 @@ h1
 const h1 = document.querySelector("h1").innerText;
 const h2 = document.querySelectorAll("h2")[0].innerText;
 const liFirst = document.getElementsByTagName("li")[0].innerText;
-const labeltwo = document.querySelector(".cat-photo").innerText;
+const labeltwo = document.querySelector("lable[for='cat-photo']").innerText;
 
 console.log(`${h1}\n${h2}\n${liFirst}\n${labeltwo}`);
