004c223ecaa149f4dc54475d5904decfe65c0550
admin <Janpole12@yandex.ru>
2024-03-12 22:32:32 +0300

    3-8-1

--- tasks/3-8-1/main.js
+++ tasks/3-8-1/main.js
@@ -5,3 +5,9 @@ h1
 первого элемента списка li
 второго label (Фото котика/собаки)
 */
+const h1 = document.querySelector("h1");
+const h2 = document.querySelectorAll("h2")[0];
+const liFirst = document.querySelector("cat-list")[0];
+const labeltwo = document.querySelector(".cat-photo");
+
+console.log(`${h1}\n${h2}\n${liFirst}\n${labeltwo}`);
