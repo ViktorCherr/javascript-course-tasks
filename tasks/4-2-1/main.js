2ec4595ac7c108b64a5f35d73d925e8d218f3627
admin <Janpole12@yandex.ru>
2024-03-14 19:35:10 +0300

    4-2-1

--- tasks/4-2-1/main.js
+++ tasks/4-2-1/main.js
@@ -4,7 +4,7 @@
 const button = document.querySelector("button");
 const inp1 = document.querySelector("[name='input-1']");
 const inp2 = document.querySelector("[name='input-2']");
-const res = document.querySelector("#res");
+const res = document.querySelector("#result");
 
 button.addEventListener("click", function(){
     res.textContent = +inp1.value + +inp2.value;
