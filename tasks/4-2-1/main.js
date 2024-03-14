ef4978202d8e440f4d1dc2738b466125f085e6e2
admin <Janpole12@yandex.ru>
2024-03-14 18:26:36 +0300

    4-2-1

--- tasks/4-2-1/main.js
+++ tasks/4-2-1/main.js
@@ -1,3 +1,12 @@
 /*
 Вам нужно написать js код, который выведет сумму введенных в input чисел.
 */
+const button = document.querySelector("button");
+const inp1 = document.querySelector("[name='input-1']");
+const inp2 = document.querySelector("[name='input-2']");
+const res = document.querySelector("#res");
+
+button.addEventListener("click", function(){
+    res.textContent = +inp1.value + +inp2.value;
+});
+
