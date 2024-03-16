f587c8829b3c4fe255e823dd673896aca7dab359
admin <Janpole12@yandex.ru>
2024-03-16 16:09:43 +0300

    5-2-1

--- tasks/5-2-1/main.js
+++ tasks/5-2-1/main.js
@@ -4,3 +4,15 @@
 
 Важно: пользователь может ввести лишние пробелы - это должно отслеживаться.
  */
+const spanElem = document.querySelector("span");
+const inpName = document.querySelector("#name");
+const inpSurName = document.querySelector("#surname");
+const inpPatr = document.querySelector("#patronymic");
+const buttElem = document.querySelector("button");
+
+
+// let res = `${inpSurName.value.trim()} ${inpName.value.trim()[0].toUpperCase()}. ${inpPatr.value.trim()[0].toUpperCase()}.`;
+
+buttElem.addEventListener("click", function(){
+    spanElem.textContent = `${inpSurName.value.trim()} ${inpName.value.trim()[0].toUpperCase()}. ${inpPatr.value.trim()[0].toUpperCase()}.`;
+});
