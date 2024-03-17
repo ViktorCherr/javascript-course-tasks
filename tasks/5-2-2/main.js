99fb08d479181d99035f3776d3b5af3e855c82c9
admin <Janpole12@yandex.ru>
2024-03-17 15:52:31 +0300

    5-2-2

--- tasks/5-2-2/main.js
+++ tasks/5-2-2/main.js
@@ -5,3 +5,21 @@
 
 Вы также можете усложнить задание и добавить удаление пользователя.
  */
+const inpName = document.querySelector("#name");
+const inpSurName = document.querySelector("#surname");
+const inpPatr = document.querySelector("#patronymic");
+const list = document.querySelector("#user-list");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    let li = document.createElement("li");
+    li.innerHTML = `Пользователь: ${inpSurName.value.trim()}
+        ${inpName.value.trim()[0].toUpperCase()}. ${inpPatr.value.trim()[0].toUpperCase()}.
+        <button onclick="removeItem(this)">Удалить</button>`;
+
+    list.appendChild(li);
+});
+
+function removeItem(button){
+    list.removeChild(button.parentNode);
+}
