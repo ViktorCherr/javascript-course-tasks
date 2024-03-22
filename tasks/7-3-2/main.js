88290a7f60cc499627f4f5edbb249a8e2419e2bd
admin <Janpole12@yandex.ru>
2024-03-22 21:46:48 +0300

    7-3-2

--- tasks/7-3-2/main.js
+++ tasks/7-3-2/main.js
@@ -5,3 +5,23 @@
 Таблица должна быть представлена в виде HTML-таблицы, 
 где значения в ячейках - результаты умножения числа на числа от 1 до 10.
  */
+let num = document.querySelector("input");
+let btn = document.querySelector(".btn");
+let tableCont = document.querySelector("#table-container");
+
+btn.addEventListener("click", function(){
+    let number = parseInt(num.value);
+    if (number > 0 && number <= 10) {
+        tableCont.innerHTML = "";
+        table = document.createElement("table");
+        for (let i = 1; i <= 10; i++) {
+            let tableRow = document.createElement("tr");
+            tableRow.innerHTML = `<td>${number} * ${i}</td><td>${number * i}</td>`;
+            table.appendChild(tableRow);
+        }
+        tableCont.appendChild(table);
+    } else {
+        alert("Введите число от 1 до 10!");
+        num.value = "";
+    }
+});
