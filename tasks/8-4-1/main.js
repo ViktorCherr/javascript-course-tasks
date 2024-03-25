e1ccf0eeb0ecbf553fb4dc875b0f55b62d3e80e7
admin <Janpole12@yandex.ru>
2024-03-25 20:55:19 +0300

    8-4-1

--- tasks/8-4-1/main.js
+++ tasks/8-4-1/main.js
@@ -4,3 +4,20 @@
 - скрипт, который при нажатии на кнопку "Купить" добавляет соответствующий товар в корзину.
 - функцию для удаления товара из корзины.
 */
+document.querySelectorAll(".buy-button").forEach((button) => {
+    button.addEventListener("click", addToBasket);
+})
+
+function addToBasket(event) {
+    const parent = event.target.parentNode;
+    let prodName = parent.querySelector(".product-name").textContent;
+    let li = document.createElement("li");
+    li.className = 'cart-item';
+    li.innerHTML = `<span>${prodName}</span><button onclick='delElem(this)'>Удалить</button>`
+    document.querySelector("#cart-list").appendChild(li);
+}
+
+function delElem(button) {
+    parent = button.parentNode;
+    document.querySelector("#cart-list").removeChild(parent);
+}
