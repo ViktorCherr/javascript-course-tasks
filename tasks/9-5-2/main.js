104c08b7c7d9ee93fcee293046579e333a0418a9
admin <Janpole12@yandex.ru>
2024-03-30 16:16:49 +0300

    9-5-2

--- tasks/9-5-2/main.js
+++ tasks/9-5-2/main.js
@@ -11,9 +11,14 @@ const students = [
     'Наталья',
     'Ольга',
     'Сергей',
-    // ... добавьте еще имен по желанию
+    'Виктор',
+    'Ибрагим',
+    'Анатолий',
+    'Владислав',
+    'Григорий',
+    'Мария',
 ];
-
+const ulElem = document.querySelector('#student-list');
 // Назначаем обработчик события ввода на поле
 document.getElementById('filter-input').addEventListener('input', handleFilter);
 
@@ -28,19 +33,28 @@ function handleFilter(event) {
     clearStudentsList();
 
     // Перебираем массив "students" и отображаем только отфильтрованных студентов
+    students.forEach(student => {
+        if (student.toLowerCase().startsWith(filterValue)) {
+            addStudentToList(student);
+        }
+    })
 }
 
 // Функция отображения всех студентов
 function showStudents(studentsArray) {
-
+    for (let student of studentsArray) {
+        addStudentToList(student);
+    }
 }
 
 // Функция добавления студента в список на веб-странице
 function addStudentToList(student) {
-
+    let li = document.createElement('li');
+    li.textContent = student;
+    ulElem.appendChild(li);
 }
 
 // Функция очистки списка студентов на веб-странице
 function clearStudentsList() {
-
+    ulElem.innerHTML = '';
 }
