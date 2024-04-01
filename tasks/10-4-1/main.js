4929f04b329669dea48688419dba087af7a5aa38
admin <Janpole12@yandex.ru>
2024-04-01 22:04:06 +0300

    10-4-1

--- tasks/10-4-1/main.js
+++ tasks/10-4-1/main.js
@@ -8,4 +8,25 @@
 быть сохранены в объекте. 
 После сохранения, отобразите сохраненные данные на странице.
 */
-  
+
+function userData(userName, email, age) {
+    this.userName = userName;
+    this.email = email;
+    this.age = age;
+}
+
+function saveUserData(){
+    const nameInp = document.querySelector('#name');
+    const emailInp = document.querySelector('#email');
+    const ageInp = document.querySelector('#age');
+    if (nameInp.value !== '' && emailInp.value !== '' && ageInp.value !== '') {
+        let userObj = new userData(nameInp.value, emailInp.value, ageInp.value);
+        for (let field of [nameInp, emailInp, ageInp]) {
+            field.value = '';
+        }
+        document.querySelector('#outputName').textContent = userObj.userName;
+        document.querySelector('#outputEmail').textContent = userObj.email;
+        document.querySelector('#outputAge').textContent = userObj.age;
+        document.querySelector('#output').className = '';
+    } else alert('Заполните все поля');
+}
