e01d81071ad0f96955e9e14d59608351a4020aba
admin <Janpole12@yandex.ru>
2024-04-02 21:42:11 +0300

    10-4-2

--- tasks/10-4-2/main.js
+++ tasks/10-4-2/main.js
@@ -24,3 +24,49 @@ const characters = {
     numbers: "0123456789",
     symbols: "!$%&|[](){}:;.,*+-#@<>~"
 }
+
+let selectedItems = [];
+const resultField = document.querySelector('#result');
+const copyBtn = document.querySelector('#copy-btn');
+
+document.querySelector('.generate-btn').addEventListener('click', function(){
+    document.querySelectorAll('input[type=checkbox]').forEach(inp => {
+        if (inp.checked) {
+            selectedItems.push(inp.id);
+        }
+    })
+    symbolsString = characters.lowercase;
+    symbolsString += selectedItems.includes('spaces') ? ' ' : '';
+    for (let item of selectedItems) {
+        if (item !== 'spaces' || item !== 'exc-duplicate') {
+            symbolsString += characters[item];
+        }
+    }
+    resultField.value = getPassword(symbolsString);
+})
+
+function getPassword(str) {
+    const passwordLength = document.querySelector('#length').value;
+    let result = '';
+    if (passwordLength !== '' && +passwordLength >= 5 && +passwordLength <= 20) {
+        while (result.length !== +passwordLength) {
+            let symbol = str[Math.round(Math.random() * str.length)];
+            if (selectedItems.includes('exc-duplicate')) {
+                result += result.includes(symbol) ? '' : symbol;
+            } else {
+                result += symbol;
+            }
+        }
+    } else {
+        alert('Укажите длину пароля от 5 до 20 символов!');
+    }
+    document.querySelector('#length').value = '';
+    return result;
+}
+
+copyBtn.addEventListener('click', function(){
+    copyBtn.textContent = 'done_all';
+    copyBtn.style.color = '#4CAF50';
+    navigator.clipboard.writeText(resultField.value);
+    setTimeout(() => {copyBtn.textContent = 'content_copy'; copyBtn.style.color = '#707070'}, 5000);
+})
