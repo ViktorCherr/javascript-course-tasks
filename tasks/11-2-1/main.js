fb2dc59facbe245871ea169ca0cb3a8f1ad9a938
admin <Janpole12@yandex.ru>
2024-04-04 21:57:24 +0300

    11-2-1

--- tasks/11-2-1/main.js
+++ tasks/11-2-1/main.js
@@ -6,3 +6,34 @@
 
 Также пользователь может удалять дела из списка дел.
 */
+const tasks = new Set();
+
+function addTask(task) {
+    const inp = document.querySelector('#taskInput');
+    let val = inp.value;
+    if (val !== '') {
+        if (tasks.has(val)) {
+            alert('Эта задача уже есть в списке!');
+        }
+        tasks.add(val);
+        inp.value = '';
+        showTasks();
+    } else {
+        alert('Заполните поле!');
+    }
+}
+
+function showTasks(){
+    const list = document.querySelector('#taskList');
+    list.innerHTML = '';
+    tasks.forEach(task => {
+        let li = document.createElement('li');
+        li.innerHTML = `${task}<button class='del-btn' onclick='delTask(this)'>Удалить</button>`;
+        list.appendChild(li);
+    })
+}
+
+function delTask(task){
+    tasks.delete(task.parentElement.textContent.slice(0, -7));
+    showTasks();
+}
